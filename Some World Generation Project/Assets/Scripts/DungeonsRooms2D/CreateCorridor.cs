59a919cf52dfb591a1fc2d111670786ca948fe4a
SEGMK <segmk.wilder@gmail.com>
2022-06-04 17:15:33 +0200

    CreateCorridor fix
    Major change of ScanMapAxiesToFindNewPosition method and minor fixes of other methods in the class.

--- Some World Generation Project/Assets/Scripts/DungeonsRooms2D/CreateCorridor.cs
+++ Some World Generation Project/Assets/Scripts/DungeonsRooms2D/CreateCorridor.cs
@@ -6,25 +6,20 @@ public class CreateCorridor : ScriptableObject
 {
     public GameObject[] CorridorTiles;
     [SerializeField] private int MaxCorridorLeangth = 20;
-    [SerializeField] private int ChanceToEndCreationOfCorridor = 10;
-    [SerializeField] private int NumberOfCorridors = 10;
-    private (int, int) MapSize;
-    [SerializeField] private int ChanceToCreateCorridorSideways = 2;
+    [SerializeField] private int ChanceToEndCreationOfCorridor = 20;
+    [SerializeField] private int NumberOfCorridors = 30;
+    [SerializeField] private int ChanceToCreateCorridorSideways = 4;
     public GameObject[,] CreateCorridorMap(GameObject[,] map)
     {
-        int dimensionX = map.GetLength(0);
-        int dimensionY = map.GetLength(1);
-        MapSize = (dimensionX - 1, dimensionY - 1);
-        (int, int) startingPoint = (Random.Range(0, dimensionX), Random.Range(0, dimensionY));
+        ((int, int), bool) startingPointAndDirection = ((Random.Range(1, map.GetLength(0) - 1), Random.Range(1, map.GetLength(1) - 1)), true);
         for (int i = 0; i < NumberOfCorridors; i++)
         {
-            GenerateCorridor(ref map, startingPoint);
-            startingPoint = FindNewStartingPosition(map);
+            GenerateCorridor(ref map, startingPointAndDirection.Item1, startingPointAndDirection.Item2);
+            startingPointAndDirection = FindNewStartingPosition(map);
         }
-        DebugThing(map);
         return map;
     }
-    private void DebugThing(GameObject[,] map)
+    private void DebugThing(GameObject[,] map) //delete it
     {
         foreach (var i in map)
         {
@@ -33,56 +28,59 @@ public class CreateCorridor : ScriptableObject
                 Debug.Log("Hit");
             }
         }
+        Debug.Log("End of the map");
     }
-    private (int, int) FindNewStartingPosition(GameObject[,] map)
+    private ((int, int), bool) FindNewStartingPosition(GameObject[,] map)
     {
-        int scanStartingPosition = Random.Range(0, MapSize.Item1);
-        if(Random.Range(1, ChanceToCreateCorridorSideways) != 1)
-            return ScanMapAxiesToFindNewPosition(scanStartingPosition, map, MapSize.Item1, MapSize.Item2, true);
+        if (Random.Range(1, ChanceToCreateCorridorSideways) == 1)
+        {
+            return (ScanMapAxiesToFindNewPosition(map, map.GetLength(0), map.GetLength(1), true), true);
+        }
         else
-            return ScanMapAxiesToFindNewPosition(scanStartingPosition, map, MapSize.Item2, MapSize.Item1, false);
+        {
+            return (ScanMapAxiesToFindNewPosition(map, map.GetLength(1), map.GetLength(0), false), false);
+        }
     }
-    private (int, int) ScanMapAxiesToFindNewPosition(int scanStartingPosition, GameObject[,] map, int lengthOfAxisWhereMethodIsScanningAlong, 
+    private (int, int) ScanMapAxiesToFindNewPosition(GameObject[,] map, int lengthOfAxisWhereMethodIsScanningAlong, 
         int lengthofScannedAxies, bool isScaningAlongXAxies)
     {
-        while (true)
+        int r = Random.Range(0, lengthOfAxisWhereMethodIsScanningAlong - 1);
+        for (int i = r; i < lengthOfAxisWhereMethodIsScanningAlong;)
         {
-            for (int i = 0; i <= lengthOfAxisWhereMethodIsScanningAlong / 10; i++) //how many scansc are made every 10 tiles
+            int r2 = Random.Range(0, lengthofScannedAxies - 1);
+            for (int j = r2; j < lengthofScannedAxies;)
             {
-                for (int j = 0; j < lengthofScannedAxies; j++) //scan whole axies row at scanPosition position
+                if (isScaningAlongXAxies)
                 {
-                    switch (isScaningAlongXAxies) //try to write it clearer //if...else is killing my eyes here thats why I did it in switch...case at the moment
+                    if (map[i, j] != null)
                     {
-                        case true:
-                            if (map[scanStartingPosition, j] != null)
-                            {
-                                return (scanStartingPosition, j);
-                            }
-                            break;
-                        case false:
-                            if (map[j, scanStartingPosition] != null)
-                            {
-                                return (j, scanStartingPosition);
-                            }
-                            break;
+                        return (i, j);
                     }
-                }
-                if (lengthOfAxisWhereMethodIsScanningAlong < scanStartingPosition + 10)
-                {
-                    scanStartingPosition = (scanStartingPosition + 10) - lengthOfAxisWhereMethodIsScanningAlong;
                 }
                 else
                 {
-                    scanStartingPosition += 10;
+                    if (map[j, i] != null)
+                    {
+                        return (j, i);
+                    }
                 }
+                j++;
+                if (lengthofScannedAxies == j)
+                    j = 0;
+                if (j == r2)
+                    break;
             }
-            scanStartingPosition += 1;
+            i++;
+            if (lengthOfAxisWhereMethodIsScanningAlong == i)
+                i = 0;
+            if (i == r)
+                break;
         }
+        throw new System.Exception("Map seems empty");
     }
-    private void GenerateCorridor(ref GameObject[,] map, (int, int) startingPoint)
+    private void GenerateCorridor(ref GameObject[,] map, (int, int) startingPoint, bool sidewayes)
     {
         bool positiveWay = System.Convert.ToBoolean(Random.Range(0, 1));
-        bool VerticalWay = map[startingPoint.Item1 - 1, startingPoint.Item2] != null || map[startingPoint.Item1 + 1, startingPoint.Item2] != null;
         for (int i = 1; i <= MaxCorridorLeangth; i++)
         {
             if (Random.Range(1, ChanceToEndCreationOfCorridor) == 1)
@@ -90,7 +88,7 @@ public class CreateCorridor : ScriptableObject
             try
             {
                 //doing it this way cuz these are all possibilities
-                if (VerticalWay)
+                if (!sidewayes)
                 {
                     if (positiveWay)
                     {
